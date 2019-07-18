db30f1ae4cbd6144d3021fe3bb68fd4711408c3b
liuzhe <zhethink@gmail.com>
2019-07-18 15:21:32 +0800

    完成button-group

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import button from './button'
 import icon from './icon'
+import buttonGroup from './button-group'
 
 Vue.component('z-button', button);
 Vue.component('z-icon', icon);
+Vue.component('z-button-group', buttonGroup);
 new Vue({
   el: '#app',
   data: {
---
+++ src/button-group.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="z-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "button-group"
+  }
+</script>
+
+<style scoped lang="sass">
+  .z-button-group
+    display: inline-flex
+    .z-button
+      border-radius: 0
+      margin-left: -1px
+      &:first-child
+        border-bottom-left-radius: var(--border-radius)
+        border-top-left-radius: var(--border-radius)
+      &:last-child
+        border-bottom-right-radius: var(--border-radius)
+        border-top-right-radius: var(--border-radius)
+      &:hover
+        position: relative
+        z-index: 1
+</style>
