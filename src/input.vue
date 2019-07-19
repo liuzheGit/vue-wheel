3e830c31051f74cde2368f67d8acd4a09b2c829d
liuzhe <zhethink@gmail.com>
2019-07-19 15:58:28 +0800

    添加满足v-model的代码和测试用例

--- src/input.vue
+++ src/input.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="wrapper" :class="{error}">
     <input type="text" :value="value" :disabled="disabled" :readonly="readonly"
-      @input="$emit('input', $event)"
-      @change="$emit('change', $event)"
-      @focus="$emit('focus', $event)"
-      @blur="$emit('blur', $event)"
+      @input="$emit('input', $event.target.value)"
+      @change="$emit('change', $event.target.value)"
+      @focus="$emit('focus', $event.target.value)"
+      @blur="$emit('blur', $event.target.value)"
     >
     <template v-if="error">
       <z-icon name="error" class="error-icon"></z-icon>
