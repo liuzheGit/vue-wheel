671ec1805d1f294746f4fc18239e0db57e925b41
liuzhe <zhethink@gmail.com>
2019-07-23 18:30:59 +0800

    toast添加样式

--- src/toast.vue
+++ src/toast.vue
@@ -6,7 +6,31 @@
 
 <script>
   export default {
-    name: "z-toast"
+    name: "z-toast",
+    props: {
+      autoClose: {
+        type: Boolean,
+        default: true
+      },
+      autoCloseDelay: {
+        type: Number,
+        default: 3
+      }
+    },
+    mounted() {
+      let {autoClose, close, autoCloseDelay} = this;
+      if (autoClose) {
+        setTimeout(() => {
+          close();
+        }, autoCloseDelay * 1000)
+      }
+    },
+    methods: {
+      close(){
+        this.$el.remove();
+        this.$destroy()
+      }
+    }
   }
 </script>
 
@@ -14,10 +38,20 @@
   $font-size: 14px;
   $toast-min-height: 40px;
   $toast-bg: rgba(0, 0, 0, 0.75);
-  .toast{
-    position: fixed; top: 0; left: 50%; transform: translateX(-50%);
-    font-size: $font-size; min-height: $toast-min-height; display: flex; line-height: 1.8;
-    color: white; align-items: center; background: $toast-bg; border-radius: 4px;
-    box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50); padding: 0 16px;
+  .toast {
+    position: fixed;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    font-size: $font-size;
+    min-height: $toast-min-height;
+    display: flex;
+    line-height: 1.8;
+    color: white;
+    align-items: center;
+    background: $toast-bg;
+    border-radius: 4px;
+    box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50);
+    padding: 0 16px;
   }
 </style>
