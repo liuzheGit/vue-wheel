fc167c6d3e2be83d6d5199f5b303caf587ecec83
liuzhe <zhethink@gmail.com>
2019-07-23 15:51:40 +0800

    添加alide关闭动画

--- src/aside.vue
+++ src/aside.vue
@@ -1,15 +1,39 @@
 <template>
-  <div class="aside">
-    <slot></slot>
-  </div>
+  <transition name="slide">
+    <div class="aside" v-if="visible">
+      <slot></slot>
+      <button class="close" @click="visible = !visible">X</button>
+    </div>
+  </transition>
 </template>
 
 <script>
   export default {
-    name: "z-aside"
+    name: "z-aside",
+    data(){
+      return {
+        visible: true
+      }
+    }
   }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  .aside{
+    position: relative;
+    .close{
+      position: absolute;
+      top: 0;
+      right: 0;
+      padding: 4px 16px;
+    }
+  }
 
+  .slide-enter-active, .slide-leave-active {
+    transition: all .5s;
+  }
+  .slide-enter, .slide-leave-to{
+    margin-left: -200px;
+    opacity: 0;
+  }
 </style>
